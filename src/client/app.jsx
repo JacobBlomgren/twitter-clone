0281bbdf95301de1473ecdcab442a8ee5cff7853
Jacob Blomgren <jacobblo@kth.se>
2017-07-29 12:15:30 +0200

    added personal custom boilerplate code
    Boilerplate code modified from
    https://github.com/verekia/js-stack-from-scratch.

---
+++ src/client/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Jacob!</h1>
+      <p>Text text text!!!</p>
+      <p>More text</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/client/index.jsx
@@ -0,0 +1,28 @@
+import 'babel-polyfill';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from './app';
+import { APP_CONTAINER_SELECTOR } from '../shared/config';
+
+const rootEl = document.querySelector(APP_CONTAINER_SELECTOR);
+
+function wrapApp(AppComponent) {
+  return (
+    <AppContainer>
+      <AppComponent />
+    </AppContainer>
+  );
+}
+
+ReactDOM.render(wrapApp(App), rootEl);
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    // eslint-disable-next-line global-require
+    const NextApp = require('./app').default;
+    ReactDOM.render(wrapApp(NextApp), rootEl);
+  });
+}
