38be1fcab5aa0d1d66fe4b786373350244447185
Jacob Blomgren <jacobblo@kth.se>
2017-08-04 17:09:38 +0200

    added unit tests for hashtag parsing

---
+++ __tests__/unit/hashtags.js
@@ -0,0 +1,28 @@
+import findHashtags from '../../src/shared/tweet/findHashtags';
+
+it('should match a hashtag', () => {
+  expect(findHashtags('#hashtag')).toEqual(['hashtag']);
+});
+
+it('should match several hashtags with text in between', () => {
+  expect(findHashtags('#hashtag text #another #HASHTAG#oneMore')).toEqual([
+    'hashtag',
+    'another',
+    'HASHTAG',
+    'oneMore',
+  ]);
+});
+
+it('should match hashtags with non-english characters', () => {
+  expect(findHashtags('#igår')).toEqual(['igår']);
+  expect(findHashtags('#mañana')).toEqual(['mañana']);
+  expect(findHashtags('#завтра')).toEqual(['завтра']);
+});
+
+it('should not match the part after punctuation', () => {
+  expect(findHashtags('#hash.tag')).toEqual(['hash']);
+  expect(findHashtags('#hash,tag')).toEqual(['hash']);
+  expect(findHashtags('#hash¿tag')).toEqual(['hash']);
+  expect(findHashtags('#hash[tag')).toEqual(['hash']);
+  expect(findHashtags('#hash{}tag')).toEqual(['hash']);
+});
