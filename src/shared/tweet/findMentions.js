c48c487efc7a43d23b4a6f2c9ad2a9a1d323cd6b
Jacob Blomgren <jacob.blomgren@hotmail.com>
2017-08-08 12:30:59 +0200

    made mention rules more sensible

--- src/shared/tweet/findMentions.js
+++ src/shared/tweet/findMentions.js
@@ -1,27 +1,24 @@
 import R from 'ramda';
 
-const regex = '@[a-zA-z0-9]+';
+const regex = /@[a-zA-z0-9]{1,15}/g;
 
 const matchMention = R.match(regex);
 
-const checkLength = mention => mention.length <= 15;
-
-const matches = R.compose(
-  R.filter(checkLength),
-  R.map(R.replace(/@/g, '')),
-  matchMention,
-);
+const matches = R.compose(R.map(R.replace(/@/g, '')), matchMention);
 
 /**
- * Finds all username mentions in a string. Enforces the app-wide 15 character username limit,
- * and filters out such matches.
+ * Finds all username mentions in a string. If a mention breaks a username rule,
+ * everything up to the character where the rule is broken will be matched. A single "@"
+ * followed by a non-username character will not be matched
  *
  * @param {string} str - The string to find matches in.
  * @returns {Array} - The array of matches. Empty if no matches are found.
  *
  * @example
  * findMentions('@jacob'); // => ['jacob']
- * findMentions('@longerThanFifteen'); // => []
+ * findMentions('@longerThanFifteen'); // => ['longerThanFifte']
+ * findMentions('@aaá'); // => ['aa]
+ * findMentions('@'); // => []
  */
 export default function(str) {
   const res = matches(str);
