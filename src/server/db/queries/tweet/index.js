77924334b2ccde09091b07028ddd7bf4d7b443f1
Jacob Blomgren <jacob.blomgren@hotmail.com>
2017-08-12 15:03:56 +0200

    added task connection to database

--- src/server/db/queries/tweet/index.js
+++ src/server/db/queries/tweet/index.js
@@ -3,13 +3,13 @@ import R from 'ramda';
 import { db, pgpHelpers } from '../../connection';
 import getQueryFile from '../../getQueryFile';
 
-async function insertHashtags(tweetID, hashtags) {
+async function insertHashtags(tweetID, hashtags, task) {
   if (R.isEmpty(hashtags)) return Promise.resolve();
   const hashtagsWithTweetID = hashtags.map(hashtag => ({
     tweet_id: tweetID,
     hashtag,
   }));
-  return db.none(
+  return task.none(
     pgpHelpers.insert(
       hashtagsWithTweetID,
       ['tweet_id', 'hashtag'],
@@ -20,20 +20,20 @@ async function insertHashtags(tweetID, hashtags) {
 
 const mentionsQueryFile = getQueryFile('tweet/mentions');
 
-async function insertMentions(tweetID, mentions) {
+async function insertMentions(tweetID, mentions, task) {
   if (R.isEmpty(mentions)) return Promise.resolve();
   const queries = mentions.map(username => ({
     query: mentionsQueryFile,
     values: [tweetID, username],
   }));
-  return db.none(pgpHelpers.concat(queries));
+  return task.none(pgpHelpers.concat(queries));
 }
 
 const replyToQueryFile = getQueryFile('tweet/reply_to');
 
-async function insertReplyTo(tweetID, replyTo) {
+async function insertReplyTo(tweetID, replyTo, task) {
   if (replyTo === null) return Promise.resolve();
-  return db.none(replyToQueryFile, [tweetID, replyTo]);
+  return task.none(replyToQueryFile, [tweetID, replyTo]);
 }
 
 export async function insertTweet(
@@ -43,29 +43,33 @@ export async function insertTweet(
   mentions,
   replyTo,
 ) {
-  const insertion = await db.one(
-    'INSERT INTO tweet (user_id, content) VALUES ($/userID/, $/content/) RETURNING id',
-    {
-      userID,
-      content,
-    },
-  );
-  const tweetID = insertion.id;
+  // Use a task to share the database connection
+  // See https://github.com/vitaly-t/pg-promise/wiki/chaining-queries
+  return db.task('insert tweet', async task => {
+    const insertion = await task.one(
+      'INSERT INTO tweet (user_id, content) VALUES ($/userID/, $/content/) RETURNING tweet_id',
+      {
+        userID,
+        content,
+      },
+    );
+    const tweetID = insertion.tweet_id;
 
-  // These three queries could be concatenated together so that they do not make
-  // three separate database requests, as suggested here
-  // https://github.com/vitaly-t/pg-promise/wiki/Performance-Boost
-  // But since this app will never need that kind of performance optimization,
-  // it is left like this for clarity.
-  await Promise.all([
-    insertHashtags(tweetID, hashtags),
-    insertMentions(tweetID, mentions),
-    insertReplyTo(tweetID, replyTo),
-  ]);
-  return tweetID;
+    // These three queries could be concatenated together so that they do not make
+    // three separate database requests, as suggested here
+    // https://github.com/vitaly-t/pg-promise/wiki/Performance-Boost
+    // But since this app will never need that kind of performance optimization,
+    // it is left like this for clarity.
+    await Promise.all([
+      insertHashtags(tweetID, hashtags, task),
+      insertMentions(tweetID, mentions, task),
+      insertReplyTo(tweetID, replyTo, task),
+    ]);
+    return tweetID;
+  });
 }
 
-export function removeTweet(tweetID, userID) {
+export async function removeTweet(tweetID, userID) {
   return db.oneOrNone(
     'DELETE FROM tweet WHERE tweet_id = $/tweetID/ AND user_ID = $/userID/ RETURNING *',
     {
