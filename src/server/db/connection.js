64df26b5b0f6b98c4a4a3c8bd37689ca5d7d5ae0
Jacob Blomgren <jacob.blomgren@hotmail.com>
2017-08-16 19:58:16 +0200

    added minor configuration changes

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -7,6 +7,7 @@ const config = {
   database: process.env.NODE_ENV !== 'test' ? 'twitter' : 'twitter_test',
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  client_encoding: 'UTF8',
 };
 const db = pgpInitialized(config);
 const pgpHelpers = pgpInitialized.helpers;
