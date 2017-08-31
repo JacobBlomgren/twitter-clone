036544f38f458a2ed247f2f654a5b2eebe050c0a
Jacob Blomgren <jacob.blomgren@hotmail.com>
2017-08-31 13:45:29 +0200

    added recursive query for replies to getTweet

--- src/server/db/queries/tweet/getTweet/getTweetWithTask.js
+++ src/server/db/queries/tweet/getTweet/getTweetWithTask.js
@@ -1,22 +1,29 @@
+import R from 'ramda';
+
 import getQueryFile from '../../../getQueryFile';
 
 const getTweetQueryFile = getQueryFile('tweet/getTweet/get_tweet');
 const getHashtagsQueryFile = getQueryFile('tweet/getTweet/get_hashtags');
 const getMentionsQueryFile = getQueryFile('tweet/getTweet/get_mentions');
 const getReplyToQueryFile = getQueryFile('tweet/getTweet/get_reply_to');
+const getRepliesQueryFile = getQueryFile('tweet/getTweet/get_replies');
 
 export default async function(task, tweetID) {
   const tweet = await task.oneOrNone(getTweetQueryFile, tweetID);
   if (tweet === null) return Promise.resolve(null);
-  const [replyTo, hashtags, mentions] = await Promise.all([
+  const [replyTo, replies, hashtags, mentions] = await Promise.all([
     task.oneOrNone(getReplyToQueryFile, tweetID),
+    task.any(getRepliesQueryFile, tweetID),
     task.manyOrNone(getHashtagsQueryFile, tweetID),
     task.manyOrNone(getMentionsQueryFile, tweetID),
   ]);
   return {
     ...tweet,
     reply_to: replyTo,
-    hashtags,
+    // the response is of the format [{ reply_tweet_id: 1 }, { reply_tweet_id: 2 }]
+    // so convert it to an array of just the IDs.
+    replies: R.pluck('reply_tweet_id', replies),
+    hashtags: R.pluck('hashtag', hashtags),
     mentions,
   };
 }
