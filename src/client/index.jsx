26d7b065d9b1146d857b0fedcad8a82f6a9d299a
Jacob Blomgren <jacob.blomgren@hotmail.com>
2017-09-07 20:49:15 +0200

    moved store config to separate file

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,29 +1,12 @@
-import 'bootstrap';
-
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
 
 import App from './App';
-import rootReducer from './reducers/';
-import state from './state';
-import { isProd } from '../shared/utils/isProd';
+import store from './store';
 
 const rootEl = document.getElementById('root');
-
-/* eslint-disable no-underscore-dangle */
-const composeEnhancers =
-  (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-/* eslint-enable no-underscore-dangle */
-
-const store = createStore(
-  rootReducer,
-  state,
-  composeEnhancers(applyMiddleware(thunkMiddleware)),
-);
 
 function wrapApp(AppComponent, reduxStore) {
   return (
---
+++ src/client/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+
+import rootReducer from './reducers/';
+import state from './state';
+import { isProd } from '../shared/utils/isProd';
+
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers =
+  (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+/* eslint-enable no-underscore-dangle */
+
+const store = createStore(
+  rootReducer,
+  state,
+  composeEnhancers(applyMiddleware(thunkMiddleware)),
+);
+
+export default store;
