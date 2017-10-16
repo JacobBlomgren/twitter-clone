94555c6b396dd6a1f798340e42f49b5f568dcbd0
Jacob Blomgren <jacob.blomgren@hotmail.com>
2017-10-16 20:37:31 +0200

    added tweet page to server side rendering

--- src/server/rendering/controller.js
+++ src/server/rendering/controller.js
@@ -4,9 +4,17 @@ import {
   fetchProfileNotFound,
   fetchProfileSuccess,
 } from '../../shared/actions/profile';
+import getTweetWithReplies from '../db/queries/tweet/getTweet/getTweetWithReplies';
+import { fetchTweetSuccess } from '../../shared/actions/tweetDetails';
 
 export async function profile(username, loggedInUserID) {
-  const user = await getUser(undefined, username, loggedInUserID);
-  if (!user) return initStore(fetchProfileNotFound(username));
-  return initStore(fetchProfileSuccess(user));
+  const response = await getUser(undefined, username, loggedInUserID);
+  if (!response) return initStore(fetchProfileNotFound(username));
+  return initStore(fetchProfileSuccess(response));
+}
+
+export async function tweet(tweetID, loggedInUserID) {
+  const response = await getTweetWithReplies(tweetID, loggedInUserID);
+  // if (!response)
+  return initStore(fetchTweetSuccess(response));
 }
--- src/server/rendering/routes.js
+++ src/server/rendering/routes.js
@@ -2,13 +2,24 @@ import express from 'express';
 
 import renderApp from '../../../lib/server/rendering/renderApp';
 import loggedInUserID from '../routes/middleware/loggedInUserID';
-import { profile } from './controller';
+import { profile, tweet } from './controller';
 
 const router = express.Router();
 
-router.get('/u/:username', loggedInUserID, async (req, res) => {
+router.use(loggedInUserID);
+
+router.get('/u/:username', async (req, res) => {
   try {
     const store = await profile(req.params.username, req.loggedInUserID);
+    res.send(renderApp(req.url, store));
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
+router.get('/t/:tweetID', async (req, res) => {
+  try {
+    const store = await tweet(req.params.tweetID, req.loggedInUserID);
     res.send(renderApp(req.url, store));
   } catch (err) {
     res.sendStatus(500);
