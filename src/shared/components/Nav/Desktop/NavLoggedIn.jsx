68695a2ab95fd6876c05abf3c72021ed86d1d5ac
Jacob Blomgren <jacob.blomgren@hotmail.com>
2018-02-18 23:53:25 +0100

    added first draft of logged in nav for desktop

---
+++ src/shared/components/Nav/Desktop/NavLoggedIn.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Settings from 'react-icons/lib/fa/cog';
+import Home from 'react-icons/lib/fa/home';
+import Edit from 'react-icons/lib/fa/edit';
+
+import SearchbarContainer from '../../../containers/SearchbarContainer';
+
+export default function NavLoggedIn() {
+  const iconStyle = { 'vertical-align': 'inherit' };
+  const iconSize = '24px';
+  return (
+    <div className="container-fluid container-fluid--no-padding">
+      <div className="row no-gutters">
+        <div className="col-3">
+          <Link to="/">
+            <Home size={iconSize} style={iconStyle} />
+          </Link>
+          <Link to="/settings">
+            <Settings size={iconSize} style={iconStyle} />
+          </Link>
+          <Link to="/compose">
+            <Edit size={iconSize} style={iconStyle} />
+          </Link>
+        </div>
+        <div className="col-6">
+          <SearchbarContainer />
+        </div>
+        <div className="col-3" />
+      </div>
+    </div>
+  );
+}
