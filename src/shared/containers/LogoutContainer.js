5503e544620d41a6c3bbb94900071ea6afafff19
Jacob Blomgren <jacob.blomgren@hotmail.com>
2018-02-18 23:21:47 +0100

    added forgotten container

---
+++ src/shared/containers/LogoutContainer.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import { logout } from '../actions/auth';
+import Logout from '../components/Auth/Logout/Logout';
+
+function mapStateToProps(state) {
+  return {
+    fetching: state.network.auth.logout,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    logout: () => dispatch(logout()),
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Logout));
