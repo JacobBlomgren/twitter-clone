39095729eb9fac461aee06fa8bcce654cb1c4497
Jacob Blomgren <jacob.blomgren@hotmail.com>
2018-02-23 11:36:31 +0100

    added helmet rendering to server side rendering

--- src/server/rendering/serverEntry.jsx
+++ src/server/rendering/serverEntry.jsx
@@ -1,26 +1,31 @@
 import fs from 'fs';
 import React from 'react';
-import { renderToString } from 'react-dom/server';
+import ReactDOMServer from 'react-dom/server';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router-dom';
+import Helmet from 'react-helmet';
 
 import App from '../../../lib/shared/App';
 
 const index = fs.readFileSync('./dist/index.html', 'utf8');
 
+function inject(head, body) {
+  return index.replace('INJECT_HEAD', head).replace('INJECT_BODY', body);
+}
+
 export default function renderApp(location, store, routerContext = {}) {
-  const appHTML = renderToString(
+  const appHTML = ReactDOMServer.renderToString(
     <Provider store={store}>
       <StaticRouter location={location} context={routerContext}>
         <App />
       </StaticRouter>
     </Provider>,
   );
+  const serializedState = JSON.stringify(store.getState());
+  const body = `<div id="root">${appHTML}</div>
+    <script>window.__PRELOADED_STATE__ = ${serializedState}</script>`;
 
-  const html = `<div id="root">${appHTML}</div>
-    <script>
-      window.__PRELOADED_STATE__ = ${JSON.stringify(store.getState())}
-    </script>`;
+  const { title, meta } = Helmet.renderStatic();
 
-  return index.replace('INJECT', html);
+  return inject(`${title.toString()}${meta.toString()}`, body);
 }
